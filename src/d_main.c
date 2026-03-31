cf75432d2359d0867778b1e3626ce5b952c61975
Mike <69996986+retrospectmike@users.noreply.github.com>
2026-03-30 23:39:24 -0400

    goofy debug mode was left on in release sit file

--- src/d_main.c
+++ src/d_main.c
@@ -1333,15 +1333,7 @@ void D_DoomMain (void)
     // Iff additonal PWAD files are used, print modified banner
     if (modifiedgame)
     {
-	/*m*/printf (
-	    "===========================================================================\n"
-	    "ATTENTION:  This version of DOOM has been modified.  If you would like to\n"
-	    "get a copy of the original game, call 1-800-IDGAMES or see the readme file.\n"
-	    "        You will not receive technical support for modified games.\n"
-	    "                      press enter to continue\n"
-	    "===========================================================================\n"
-	    );
-	getchar ();
+	doom_log("Modified game detected (PWAD loaded)\r");
     }
 	
 
